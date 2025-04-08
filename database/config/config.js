e8d0e54b7cf86512e0eb37e1b41ddd44f286715a
Mr-Arzi <goop.robecarlos@gmail.com>
2025-04-07 20:58:09 -0600

    update mysql

--- database/config/config.js
+++ database/config/config.js
@@ -4,15 +4,15 @@ console.log("database url: ", process.env.DEV_DATABASE_URL);
 
 module.exports = {
 	development: {
-		"url": process.env.DEV_DATABASE_URL,
-		"dialect": "mysql",
+		url: process.env.DEV_DATABASE_URL,
+		dialect: 'mysql',
 	},
 	test: {
-		"url": process.env.TEST_DATABASE_URL,
-		"dialect": "mysql",
+		url: process.env.TEST_DATABASE_URL,
+		dialect: 'mysql',
 	},
 	production: {
-		"url": process.env.DATABASE_URL,
-		"dialect": "mysql",
+		url: process.env.DATABASE_URL,
+		dialect: 'mysql',
 	},
 }
